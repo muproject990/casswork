008b2811975f8515e9921edc2ddbbca66c907607
Aavash Paudel <developer.aavash@gmail.com>
2025-02-17 10:31:01 +0545

    main

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Coding Task:
+// Objective: Manage a list of people and filter by age.
+
+
+class Person
+{
+     string name="aavash p";
+     int age=23;
+    
+    public void Greet( Person p)   // method
+  {
+
+                Console.WriteLine($"Hello, my name is {p.name} and my age is  {p.age}");
+
+  }
+   
+
+
+
+    static void Main(string[] args)
+  {
+            Person p = new Person();
+            p.Greet(p);
+
+
+
+  }
+
+}
+
